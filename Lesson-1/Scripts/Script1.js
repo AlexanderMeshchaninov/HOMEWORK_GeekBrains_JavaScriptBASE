fa6a46e04cad2d936c0852f934bf6f81bc965bcd
Alexander <alexander_freeman85@mail.ru>
2021-08-10 22:30:06 +0300

    Lesson-1 added

---
+++ Lesson-1/Scripts/Script1.js
@@ -0,0 +1,5 @@
+let celciusTemperature = prompt("Введите температуру в Цельсиях : ");
+
+let fahrenheitTemperature = (9 / 5) * celciusTemperature + 32;
+
+alert("Температура по Фаренгейту : " + fahrenheitTemperature);
---
+++ Lesson-1/Scripts/Script3.js
@@ -0,0 +1,3 @@
+let expressionResult = 1000 + "108";
+
+alert("Результат выражения 1000 + '108' : " + expressionResult);
